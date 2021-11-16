813af252636769e456a9ad7f5a34432c68d66522
iluhaar <iluhaar@gmail.com>
2021-11-16 16:37:17 +0200

    changes with comments

--- src/components/CitiesList/CitiesList.jsx
+++ src/components/CitiesList/CitiesList.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Card, makeStyles, Typography } from '@material-ui/core'
-import { Link, Outlet } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeCity } from '../../app/slices/citiesSlice'
-import { useGetPokemonByNameQuery } from '../../services/pokemon'
+import React, { useEffect, useState } from 'react';
+import { Button, Card, makeStyles, Typography } from '@material-ui/core';
+import { Link, Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCity } from '../../app/slices/citiesSlice';
+import { getWeather } from '../../app/slices/fetchDataSlice';
+import axios from 'axios'
 // import { useGetCityWeather } from '../../services/cityWeather'
+const URL = 'https://api.openweathermap.org/data/2.5/weather?appid=56d61b45593dee9597decc258d63ce78&q='
 
 const useStyles = makeStyles(() => ({
     cityCard: {
@@ -14,24 +16,31 @@ const useStyles = makeStyles(() => ({
 
 const CitiesList = () => {
     const { cityCard } = useStyles();
+    const [weatherData, setWeatherData] = useState({})
     const city = useSelector((state) => state.cities)
-    const weather = useSelector((state) => state.fetchWeather)
-    const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+    const weather = useSelector((state) => state.weather)
     const dispatch = useDispatch()
-    
-    // const { data, error, isLoading } = useGetCityWeather('kyiv')
-
 
     useEffect(() => {
-        const lsCity = localStorage.getItem('Kyiv')
-        console.log('ls',lsCity)
-    },[])
+        debugger
+        axios.get(`${URL}${city}`)
+            .then(res => {
+                debugger
+                getWeather(res.data)
+                setWeatherData(res.data)
+                debugger
+            })
+        debugger
+    }, [city]);
 
 
     useEffect(() => {
         console.log('city: ', city)
-        console.log('weather: ', weather)
-    }, [city, weather])
+        console.log('weather: ', weatherData)
+        console.log('weatherSlice: ', weather)
+    }, [city, weatherData, weather])
+    
+    // if (!weatherData || weatherData.main.length === 0) return <p>Loading...</p>
 
     return (
         <div className="citiesList">
@@ -40,7 +49,8 @@ const CitiesList = () => {
                     <div className="itemCity" key={i}>
                         <Card variant="outlined" className={cityCard} >
                             <Typography variant="h3" component="h3">{city}</Typography>
-                            <Typography variant="subtitle1" component="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dignissimos nesciunt maiores est accusantium eaque eligendi? Repudiandae laudantium libero nostrum iure tenetur explicabo deserunt asperiores. Quis esse magni officiis iusto?</Typography>
+                            {/* <Typography variant="subtitle1" component="p">{weatherData.name}</Typography>
+                            <Typography variant="subtitle1" component="p">{weatherData.main.temp}</Typography> */}
                             <Button size="small"> <Link to={`cities/${city}`}> More </Link> </Button>
                             <Button size="small" onClick={() => dispatch(removeCity(city))}>  Delete</Button>
                         </Card>
